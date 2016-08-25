f0179288ac0dd1daa3a1773c6747702255831e88
abner <abner.ribeiro@tegra.me>
2016-08-24 21:20:12 -0300

    added jquery, bootstrap

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('bower_components'));
 
 app.use('/', routes);
 app.use('/users', users);
