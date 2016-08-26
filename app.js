b4d2d556a0cb4b64a75df98154febe5e31f02c29
andreluistosato <andreluistosatoo@gmail.com>
2016-08-25 22:54:05 -0300

    Home

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var home = require('./routes/home');
 var spotifyoauth  = require('./routes/spotifyoauth');
 var users = require('./routes/users');
 var shirts = require('./routes/shirts');
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static('bower_components'));
 
 app.use('/', routes);
+app.use('/home', home);
 app.use('/spotifyoauth', spotifyoauth);
 app.use('/users', users);
 app.use('/shirts', shirts);
